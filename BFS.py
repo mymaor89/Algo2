f4b591b2945834db4a7447470e23d18be67ccb8d
Maor Yatskan <mymaor89@gmail.com>
2020-10-25 00:07:59 +0300

    Start of bfs

--- BFS.py
+++ BFS.py
@@ -1,4 +1,13 @@
 import queue
-queue  = queue.Queue()
-queue.put("a")
-print(queue.get())
+import math
+def bfs(arr):
+    queue  = queue.Queue()
+    s = arr[0]
+    pi = [None] * len(arr)
+    d = [math.inf] * len(arr)
+    color = ['white'] * len(arr)
+    color[0] = 'gray'
+    while not queue.empty():
+        u = queue.get()
+        
+#print(queue.get())
